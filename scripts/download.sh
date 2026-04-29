c66dea684855e27c2c8a26c69f3290123572d1d5
Srvtox <mr.nobody.cxyz@gmail.com>
2026-04-29 07:43:57 +0330

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -10,7 +10,7 @@ cd dist
 echo "Downloading metadata..."
 curl -L -O $BASE/channel-rust-stable.toml
 
-VERSION="1.95.0"
+VERSION=$(grep -A1 '\[pkg.rust\]' channel-rust-stable.toml | grep version | cut -d '"' -f2 | cut -d ' ' -f1)
 
 echo "Rust version: $VERSION"
 echo $VERSION > VERSION
